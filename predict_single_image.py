401eda496f35ef7eedaa95b34f6d79fd0f4699f4
David Iommi <david2311@hotmail.it>
2021-11-14 19:23:37 +0100

    Update predict_single_image.py

--- predict_single_image.py
+++ predict_single_image.py
@@ -105,9 +105,9 @@ def segment(image, label, result, weights, resolution, patch_size, network, gpu_
         device = torch.device("cpu")
 
     # build the network
-    if network is 'nnunet':
+    if network == 'nnunet':
         net = build_net()  # nn build_net
-    elif network is 'unetr':
+    elif network == 'unetr':
         net = build_UNETR() # UneTR
 
     net = net.to(device)
@@ -135,7 +135,7 @@ def segment(image, label, result, weights, resolution, patch_size, network, gpu_
 
         else:
             for val_data in val_loader:
-                val_images, val_labels = val_data["image"].cuda(), val_data["label"].cuda()
+                val_images, val_labels = val_data["image"].to(device), val_data["label"].to(device)
                 val_outputs = sliding_window_inference(val_images, roi_size, sw_batch_size, net)
                 val_outputs = [post_trans(i) for i in decollate_batch(val_outputs)]
                 dice_metric(y_pred=val_outputs, y=val_labels)
